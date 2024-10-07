e1027ca3f2dda9e5c5f3118bdcaaaa34ec30b64a
Meriç ALKIŞLA <70147993+MAlkisla@users.noreply.github.com>
2024-10-07 15:26:02 +0300

    Component Structure and Routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-// import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-const App = () => (
-  <>
-    <CssBaseline />
-    <h1>Hello, World - Filmpire</h1>
-  </>
-);
+import useStyles from './styles'
+
+import { Actors, MovieInformation, NavBar, Movies, Profile } from './';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+      <div className={classes.toolbar}/>
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
 
 export default App;
