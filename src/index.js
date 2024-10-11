374c46cc7f0d2ffc655bf59c23852b55af042d3e
Meriç ALKIŞLA <70147993+MAlkisla@users.noreply.github.com>
2024-10-11 16:29:02 +0300

    Redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { ThemeProvider as StylesThemeProvider } from '@mui/styles';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import store from './app/store'
 
 const theme = createTheme({});
 const rootElement = document.getElementById('root');
@@ -12,11 +14,13 @@ const root = createRoot(rootElement);
 
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <StylesThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StylesThemeProvider>
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <StylesThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </StylesThemeProvider>
+    </ThemeProvider>
+  </Provider>
 );
