316a1e093f73544f7804288288d044ca90329236
Meriç ALKIŞLA <70147993+MAlkisla@users.noreply.github.com>
2024-10-25 11:33:32 +0300

    Redux Slice

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react';
 import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
+import { useDispatch, useSelector } from 'react-redux';
 
+import {selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 import { useGetGenresQuery } from '../../services/TMDB';
 import useStyles from './styles';
 import genreIcons from '../../assets/genres';
@@ -20,7 +22,8 @@ const Sidebar = ({ setMobileOpen }) => {
     const theme = useTheme();
     const classes = useStyles();
     const { data, isFetching } = useGetGenresQuery();
-    
+    const dispatch = useDispatch();
+
     return (
         <>
             <Link to='/' className={classes.imageLink}>
@@ -34,7 +37,7 @@ const Sidebar = ({ setMobileOpen }) => {
                 <ListSubheader>Categories</ListSubheader>
                 {categories.map(({ label, value }) => (
                     <Link key={value} className={classes.links} to='/'>
-                        <ListItem onClick={() => { }} button>
+                        <ListItem onClick={() => dispatch(selectGenreOrCategory(value))} button>
                             <ListItemIcon>
                                 <img src={genreIcons[label.toLowerCase()]} 
                                 className={classes.genreImages} 
@@ -55,7 +58,7 @@ const Sidebar = ({ setMobileOpen }) => {
                 )
                     : data.genres.map(({name, id}) => (
                 <Link key={name} className={classes.links} to='/'>
-                    <ListItem onClick={() => { }} button>
+                    <ListItem onClick={() => dispatch(selectGenreOrCategory(id))} button>
                         <ListItemIcon>
                             <img src={genreIcons[name.toLowerCase()]} 
                                 className={classes.genreImages} 
